7313a8b5419f2a80f18a68888738f35ea2fc77ae
shibbuboy99 <shibbuboy99@gmail.com>
2021-04-15 20:10:38 +0530

    garage

---
+++ js/get.js
@@ -0,0 +1,9 @@
+ var element;
+ // element =document.head;
+ // console.log(element);
+
+ // element=document.getElementById("a").innerText;
+// element=document.getElementById("a").innerHTML;
+// element=document.getElementById("a").getAttribute("style");
+// element=document.getElementById("a").getAttributeNode("style");
+element=document.getElementById("a").attributes[0];
---
+++ js/set.js
@@ -0,0 +1,7 @@
+document.getElementById("a").innerHTML = "<center><h1>PNINFOSYS GWALIOR</h1></center>"
+
+document.getElementById("b").innerHTML = "<h1>Phone:7000846823</h1>"
+
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+
+document.getElementById("a").removeAttribute("style");
